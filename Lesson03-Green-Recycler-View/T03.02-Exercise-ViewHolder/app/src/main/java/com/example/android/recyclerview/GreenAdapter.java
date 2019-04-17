a8d9e5051384f4fb3d586c668311cabac4fdb060
giorgio <giorgio@email.it>
2019-04-17 10:55:13 +0200

    esercizio 3.2 risolto

--- Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
+++ Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
@@ -117,15 +117,26 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
         return mNumberItems;
     }
 
-    // TODO (12) Create a class called NumberViewHolder that extends RecyclerView.ViewHolder
+    // DONE (12) Create a class called NumberViewHolder that extends RecyclerView.ViewHolder
+    // DONE (13) Within NumberViewHolder, create a TextView variable called listItemNumberView
 
-    // TODO (13) Within NumberViewHolder, create a TextView variable called listItemNumberView
-
-    // TODO (14) Create a constructor for NumberViewHolder that accepts a View called itemView as a parameter
-    // TODO (15) Within the constructor, call super(itemView) and then find listItemNumberView by ID
+    // DONE (14) Create a constructor for NumberViewHolder that accepts a View called itemView as a parameter
+    // DONE (15) Within the constructor, call super(itemView) and then find listItemNumberView by ID
 
     // TODO (16) Within the NumberViewHolder class, create a void method called bind that accepts an int parameter called listIndex
     // TODO (17) Within bind, set the text of listItemNumberView to the listIndex
     // TODO (18) Be careful to get the String representation of listIndex, as using setText with an int does something different
 
+    public class NumberViewHolder extends RecyclerView.ViewHolder{
+        public TextView ListItemNumberView;
+
+        public NumberViewHolder(View itemView) {
+            super(itemView);
+            ListItemNumberView = (TextView) itemView.findViewById(R.id.tv_item_number);
+        }
+
+        public void bind(int ListIndex){
+            ListItemNumberView.setText(String.valueOf(ListIndex));
+        }
+    }
 }
