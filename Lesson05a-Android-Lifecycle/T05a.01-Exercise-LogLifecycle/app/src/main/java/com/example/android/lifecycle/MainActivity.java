2e77423ab3d3fcaf576ad77c959e88614dbf8244
giorgio <giorgio@email.it>
2019-04-18 16:12:54 +0200

    esercizio 5.1 risolto

--- Lesson05a-Android-Lifecycle/T05a.01-Exercise-LogLifecycle/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ Lesson05a-Android-Lifecycle/T05a.01-Exercise-LogLifecycle/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -48,19 +48,61 @@ public class MainActivity extends AppCompatActivity {
         mLifecycleDisplay = (TextView) findViewById(R.id.tv_lifecycle_events_display);
 
         // TODO (1) Use logAndAppend within onCreate
+        logAndAppend("On Create");
     }
 
     // TODO (2) Override onStart, call super.onStart, and call logAndAppend with ON_START
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        logAndAppend("On Start");
+    }
+
     // TODO (3) Override onResume, call super.onResume, and call logAndAppend with ON_RESUME
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        logAndAppend("On Resume");
+    }
+
 
     // TODO (4) Override onPause, call super.onPause, and call logAndAppend with ON_PAUSE
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        logAndAppend("On Pause");
+    }
+
+
     // TODO (5) Override onStop, call super.onStop, and call logAndAppend with ON_STOP
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        logAndAppend("On Stop");
+    }
+
 
     // TODO (6) Override onRestart, call super.onRestart, and call logAndAppend with ON_RESTART
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        logAndAppend("On Restart");
+    }
+
+
     // TODO (7) Override onDestroy, call super.onDestroy, and call logAndAppend with ON_DESTROY
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        logAndAppend("On Destroy");
+    }
 
     /**
      * Logs to the console and appends the lifecycle method name to the TextView so that you can
